9c6e19eb611476a51a36dd2757500a9602cc0b8b
chadirahme <eng.chadi@gmail.com>
2020-08-25 22:36:16 -0400

    add junit4

--- src/test/java/com/chadi/k8s/SpringbootK8sApplicationTests.java
+++ src/test/java/com/chadi/k8s/SpringbootK8sApplicationTests.java
@@ -1,24 +1,39 @@
 package com.chadi.k8s;
 
-import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.stream.IntStream;
 
+import static junit.framework.TestCase.assertNull;
+
 //@SpringBootTest
-class SpringbootK8sApplicationTests {
+//@RunWith(SpringRunner.class)
+public class SpringbootK8sApplicationTests {
 
 	//@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void testAssertNull() {
+		assertNull("should be null", null);
+	}
+
+	@Test
+	public void testNames() throws Exception {
+		Assertions.assertThat("one").isEqualTo("one");
+	}
 
 	//@Test
-	public static void main(String[] args) {
-		IntStream.range(1, 3).reduce((x, y) -> x + y)
-				.ifPresent(s -> System.out.println(s));
-
-		int res= IntStream.range(1, 3).reduce(2, (x, y) -> x + y);
-		System.out.println("res>> " + res);
-	}
+//	public static void main(String[] args) {
+//		IntStream.range(1, 3).reduce((x, y) -> x + y)
+//				.ifPresent(s -> System.out.println(s));
+//
+//		int res= IntStream.range(1, 3).reduce(2, (x, y) -> x + y);
+//		System.out.println("res>> " + res);
+//	}
 }
